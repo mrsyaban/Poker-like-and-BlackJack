2111a10fc05ed0d5c8a6178468a3623e2442b94c
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-03-11 11:33:48 +0700

    revisi rumus

--- src/Kombo/Combo.cpp
+++ src/Kombo/Combo.cpp
@@ -73,8 +73,9 @@ void Combo::pair() {
 
 void Combo::twoPair() {
     int max = 0;
+    int countPair = 0;
 
-    vector<Card> pairs;
+    vector<Card> pairs, temp;
 
     int count[14] = {0};
     for (int i = 0; i < this->cards.size(); i++) {
@@ -85,41 +86,58 @@ void Combo::twoPair() {
         if (count[i] == 2) {
             pairs.push_back(this->cards[i]);
             pairs.push_back(this->cards[i+1]);
-
-            max += pairs[i].value() + pairs[i+1].value();
-
-            this->setScore(max);
-            this->combo = pairs;
+            countPair++;
         }
+    }
+    if (countPair >= 2) {
+        temp.push_back(pairs[pairs.size()-4]);
+        temp.push_back(pairs[pairs.size()-3]);
+        temp.push_back(pairs[pairs.size()-2]);
+        temp.push_back(pairs[pairs.size()-1]);
+
+        this->combo = temp;
+
+        max += this->combo[this->combo.size()-1].value() + this->combo[this->combo.size()-2].value() + this->combo[this->combo.size()-3].value() + this->combo[this->combo.size()-4].value() + 4.14;
+        this->setScore(max);
     }
 }
 
 void Combo::threeOfAKind() {
     int max = 0;
-    int i = 0;
+    int i = 0, countThree = 0;
 
-    vector<Card> temp;
+    vector<Card> temp, three;
 
     while (i < this->length()) {
         if ( (this->cards[i].getNumber() == this->cards[i+1].getNumber()) &&  (this->cards[i].getNumber() == this->cards[i-1].getNumber()) && i > 1 && i < 6) {
-            max = this->cards[i].value() + this->cards[i+1].value() + this->cards[i-1].value();
 
-            temp.insert(temp.end(), this->cards[i]);
-            temp.insert(temp.end(), this->cards[i+1]);
-            temp.insert(temp.end(), this->cards[i-1]);
+           three.insert(three.end(), this->cards[i]);
+           three.insert(three.end(), this->cards[i+1]);
+           three.insert(three.end(), this->cards[i-1]);
 
-            this->setScore(max);
-            this->combo = temp;
+            countThree++;
         }
         i++;
+    }
+
+    if (countThree >= 1) {
+        temp.push_back(three[three.size()-3]);
+        temp.push_back(three[three.size()-2]);
+        temp.push_back(three[three.size()-1]);
+
+        this->combo = temp;
+        max += this->combo[this->combo.size()-1].value() + this->combo[this->combo.size()-2].value() + this->combo[this->combo.size()-3].value() + 8.22;
+
+        this->setScore(max);
     }
 }
 
 void Combo::straight() {
     int max = 0;
     int i = 0;
+    int countStraight = 0;
 
-    vector<Card> temp;
+    vector<Card> temp, straight;
 
     while (i < this->length()) {
         if ( i + 4 < 7 ) {
@@ -128,100 +146,165 @@ void Combo::straight() {
                 this->cards[i].getNumber() + 3 == this->cards[i+3].getNumber() &&
                 this->cards[i].getNumber() + 4 == this->cards[i+4].getNumber()) {
 
-                max = this->cards[i].value() + this->cards[i+1].value() + this->cards[i+2].value() + this->cards[i+3].value() + this->cards[i+4].value();
+                straight.insert(straight.end(), this->cards[i]);
+                straight.insert(straight.end(), this->cards[i+1]);
+                straight.insert(straight.end(), this->cards[i+2]);
+                straight.insert(straight.end(), this->cards[i+3]);
+                straight.insert(straight.end(), this->cards[i+4]);
 
-                temp.insert(temp.end(), this->cards[i]);
-                temp.insert(temp.end(), this->cards[i+1]);
-                temp.insert(temp.end(), this->cards[i+2]);
-                temp.insert(temp.end(), this->cards[i+3]);
-                temp.insert(temp.end(), this->cards[i+4]);
-
-                this->setScore(max);
-                this->combo = temp;
+                countStraight++;
             }
         }
         i++;
+    }
+
+    if (countStraight >= 1) {
+        temp.push_back(straight[straight.size()-5]);
+        temp.push_back(straight[straight.size()-4]);
+        temp.push_back(straight[straight.size()-3]);
+        temp.push_back(straight[straight.size()-2]);
+        temp.push_back(straight[straight.size()-1]);
+
+        this->combo = temp;
+
+        for (auto x : this->combo) {
+            max += x.value();
+        }
+
+        max += 12.3;
+
+        this->setScore(max);
     }
 }
 
 void Combo::flush() {
     int max = 0, count = 0;
     int j = 0, i = 0;
-    vector<Card> temp;
+    vector<Card> temp, flushCards;
+    vector<bool> flush;
+
+    for (auto x : flush) {
+        x = false;
+    }
 
     while (i < this->length()) {
         while (j < this->length() && j > i) {
             if (this->cards[i].getColor() == this->cards[j].getColor()) {
-                temp.insert(temp.begin(), this->cards[i]);
+                flush[i] = true;
+                flush[j] = true;
                 count += 1;
-
-                if (count <= 5) {
-                    temp.insert(temp.end(), this->cards[j]);
-                }
             }
-
-            if (count == 5) {
-                for (auto x : temp) {
-                    max += x.value();
-                }
-                this->combo = temp;
-                this->setScore(max);
-            }
-            j++;
         }
-        i++;
+        j++;
+    }
+    i++;
+    
+    if (count >= 5) {
+        for (i = 0; i < this->length(); i++) {
+            if (flush[i] == true) {
+                flushCards.push_back(this->cards[i]);
+            }
+        }
+
+        temp.push_back(flushCards[flushCards.size()-5]);
+        temp.push_back(flushCards[flushCards.size()-4]);
+        temp.push_back(flushCards[flushCards.size()-3]);
+        temp.push_back(flushCards[flushCards.size()-2]);
+        temp.push_back(flushCards[flushCards.size()-1]);
+
+        this->combo = temp;
+
+        for (auto x : this->combo) {
+            max += x.value();
+        }
+
+        max += 18.15;
+
+        this->setScore(max);
     }
 }
 
 void Combo::fullHouse() {
-    int max = 0;
+    int max = 0, countThree = 0, countPair = 0;
 
     vector<Card> copyCards = this->cards;
-    vector<Card> temp;
+    vector<Card> temp, three, pair;
 
-    for (int i = 0; i < copyCards.size() - 2; i++) {
+    for (int i = 0; i < copyCards.size() - 2; i++) { // Detecting three of a kind
         if (this->cards[i] == this->cards[i+1] && this->cards[i] == this->cards[i+2]) {
-            temp.insert(temp.end(), this->cards[i]);
-            temp.insert(temp.end(), this->cards[i+1]);
-            temp.insert(temp.end(), this->cards[i+2]);
-
-            max += this->cards[i].value() + this->cards[i+1].value() + this->cards[i+2].value();
+            three.insert(three.end(), this->cards[i]);
+            three.insert(three.end(), this->cards[i+1]);
+            three.insert(three.end(), this->cards[i+2]);
+            countThree++;
 
             copyCards.erase(copyCards.begin() + i, copyCards.begin() + i + 3);
 
-            for (int j = 0; j < copyCards.size(); j++) {
+            for (int j = 0; j < copyCards.size(); j++) { // Then detecting pair
                 if (cards[j] == cards[j+1]) {
-                    temp.insert(temp.end(), this->cards[j]);
-                    temp.insert(temp.end(), this->cards[j+1]);
-
-                    max += this->cards[j].value() + this->cards[j+1].value();
-                    this->combo = temp;
+                    pair.insert(pair.end(), this->cards[j]);
+                    pair.insert(pair.end(), this->cards[j+1]);
+                    countPair++;
                 }
             }
         }
     }
+
+    if (countPair >= 1 && countThree >= 1) {
+        temp.push_back(three[three.size()-3]);
+        temp.push_back(three[three.size()-2]);
+        temp.push_back(three[three.size()-1]);
+        temp.push_back(pair[pair.size()-2]);
+        temp.push_back(pair[pair.size()-1]);
+
+        this->combo = temp;
+
+        for (auto x : this->combo) {
+            max += x.value();
+        }
+
+        max += 24.04;
+
+        this->setScore(max);
+    }
+
 }
 
 void Combo::fourOfKind() {
-    int max = 0;
+    int max = 0, countFour = 0;
     int i = 0;
 
-    vector<Card> temp;
+    vector<Card> temp, four;
 
     while (i < this->length()) {
         if ( (this->cards[i].getNumber() == this->cards[i+1].getNumber()) &&  (this->cards[i].getNumber() == this->cards[i-1].getNumber()) && i + 3 < 7) {
             max = this->cards[i].value() + this->cards[i+1].value() + this->cards[i+2].value() + + this->cards[i+3].value();
 
-            temp.insert(temp.end(), this->cards[i]);
-            temp.insert(temp.end(), this->cards[i+1]);
-            temp.insert(temp.end(), this->cards[i+2]);
-            temp.insert(temp.end(), this->cards[i+3]);
+            four.insert(four.end(), this->cards[i]);
+            four.insert(four.end(), this->cards[i+1]);
+            four.insert(four.end(), this->cards[i+2]);
+            four.insert(four.end(), this->cards[i+3]);
 
-            this->setScore(max);
-            this->combo = temp;
+            countFour++;
         }
         i++;
     }
+
+    if (countFour >= 1) {
+        temp.push_back(four[four.size()-4]);
+        temp.push_back(four[four.size()-3]);
+        temp.push_back(four[four.size()-2]);
+        temp.push_back(four[four.size()-1]);
+
+        this->combo = temp;
+
+        for (auto x : this->combo) {
+            max += x.value();
+        }
+
+        max += 30.67;
+
+        this->setScore(max);
+    } 
 
 }
 
@@ -229,7 +312,7 @@ void Combo::straightFlush() {
     int max = 0;
     int i = 0;
 
-    vector<Card> temp;
+    vector<Card> temp, straight;
 
     while (i < this->length()) {
         if ( i + 4 < 7 ) {
@@ -250,13 +333,28 @@ void Combo::straightFlush() {
                     temp.insert(temp.end(), this->cards[i+2]);
                     temp.insert(temp.end(), this->cards[i+3]);
                     temp.insert(temp.end(), this->cards[i+4]);
-
-                    this->setScore(max);
-                    this->combo = temp;
                 }
             }
         }
         i++;
+    }
+
+    if (temp.size() >= 5) {
+        straight.push_back(temp[temp.size()-5]);
+        straight.push_back(temp[temp.size()-4]);
+        straight.push_back(temp[temp.size()-3]);
+        straight.push_back(temp[temp.size()-2]);
+        straight.push_back(temp[temp.size()-1]);
+
+        this->combo = straight;
+
+        for (auto x : this->combo) {
+            max += x.value();
+        }
+
+        max += 36.05;
+
+        this->setScore(max);
     }
 
 }
