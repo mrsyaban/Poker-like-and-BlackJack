e5685be30417b308f1550c605c3ac6a6bd9c2fbf
Moonawar_Laptop <moonawar@protonmail.com>
2023-03-12 12:05:10 +0700

    feat: array util template function

---
+++ src/ArrayUtil/ArrayUtil.cpp
@@ -0,0 +1,24 @@
+#ifndef ARRAY_UTIL
+#define ARRAY_UTIL
+
+// GetHighest is a recursive function that returns the highest value in an array (O(n log n))
+template <class T>
+
+// signature for array
+T getHighest(T * buffer, int size) {
+    if (size == 1) {
+        return T[0];
+    } else {
+        T left = getHighest(buffer, size / 2);
+        T right = getHighest(buffer + size / 2, size - size / 2);
+        return left > right ? left : right;
+    }
+};
+
+template <class T>
+// signature for vector
+T getHighest(vector<T> buffer) {
+    return getHighest(buffer.data(), buffer.size());
+};
+
+#endif
