364db8f1ead5b604a8786994ceb9708583efe053
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-03-14 17:47:09 +0700

    adding driverCombo

---
+++ src/Combo/driverCombo.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "Combo.hpp"
+#include "../Card/Card.hpp"
+using namespace std;
+
+// int main() {
+//     vector<int> nums = {6,4,9,3,1,8,9,4,6,0};
+// vector<int> combo = {3,4,5};
+
+//     combo = nums;
+
+//     for (auto x : combo) {
+//         cout << x << " ";
+//     }
+// }
+
+int main() {
+    // vector<Card> cards;
+    // Card card1(Green, 1);
+    // Card card2(Green, 2);
+    // Card card3(Red, 1);
+    // Card card4(Blue, 1);
+    // Card card5(Yellow, 1);
+    // Card card6(Red, 1);
+    // Card card7(Blue, 1);
+
+    // cards.push_back(card1);
+    // cards.push_back(card2);
+    // cards.push_back(card3);
+    // cards.push_back(card4);
+    // cards.push_back(card5);
+    // cards.push_back(card6);
+    // cards.push_back(card7);
+
+    // for (auto x : cards) {
+    //     cout << x.getNumber() << endl;
+    // }
+
+    // Combo coms;
+
+    
+}
