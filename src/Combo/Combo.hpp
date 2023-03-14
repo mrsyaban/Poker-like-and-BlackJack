cb15d93af80368a3e5355965629371ad13a3ea01
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-03-14 17:46:45 +0700

    Combo only value and combination

--- src/Combo/Combo.hpp
+++ src/Combo/Combo.hpp
@@ -8,45 +8,30 @@
 #include "../Table/Table.hpp"
 #include "../ObjectWithValue/ObjectWithValue.hpp"
 #include "../Exception/exception.h"
+#include "../ArrayUtil/ArrayUtil.hpp"
+#include "SearchCombo.hpp"
 
 class Combo : public ObjectWithValue<double> {
     private:
         double score;
-        vector<Card> cards;
         vector<Card> combo;
-        vector<vector<Card>> records;
 
     public:
         Combo(); // ctor
-        Combo(Player player, Table table); // constructor
+        Combo(SearchCombo combo); // constructor
         ~Combo(); // destructor
 
         double value() const; // Score Getter
         void setScore(double number); // Setter
         vector<Card> getCombo() const; // Combo Getter
         void setCombo(vector<Card>); // Combo Getter
-        vector<Card> getCards() const; // Cards Getter
-        void setCards(vector<Card>); // Cards Setter
 
-        int length(); // Length of Hand Card and Table Card
-        vector<Card> sortCards(); // Sorting cards by number and color
         vector<Card> sortCombo(); // Sorting combo by number and color
-        
-        void highCard(); 
-        void pair();
-        void twoPair();
-        void threeOfAKind();
-        void straight();
-        void flush();
-        void fullHouse();
-        void fourOfKind();
-        void straightFlush();
-
-        void checkCombo();
 
         bool operator<(const Combo& other);
         bool operator>(const Combo& other);
         bool operator==(const Combo& other);
+
 };
 
 #endif
